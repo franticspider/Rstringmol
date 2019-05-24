9bf589181d3d9c5c802816c808f686a2f2ac4748
franticspider <hickinbotham@cybula.com>
2019-05-24 17:44:59 +0100

    starting to need cpp code to run reactions without mutation

---
+++ R/Rstringmol-package.R
@@ -0,0 +1,9 @@
+## usethis namespace: start
+#' @useDynLib Rstringmol, .registration = TRUE
+## usethis namespace: end
+NULL
+
+## usethis namespace: start
+#' @importFrom Rcpp sourceCpp
+## usethis namespace: end
+NULL
