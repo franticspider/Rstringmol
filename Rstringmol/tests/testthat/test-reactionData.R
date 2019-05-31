fd3b63ba57cb2445ef4b6570eb31a72c85844a9e
franticspider <hickinbotham@cybula.com>
2019-05-31 17:23:50 +0100

    progress!

--- Rstringmol/tests/testthat/test-reactionData.R
+++ Rstringmol/tests/testthat/test-reactionData.R
@@ -3,12 +3,6 @@
 context("molecule-molecule interactions")
 
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
-
-
-
 test_that("doReaction fails gracefully",{
   result <- doReaction("")
   expect_match(result$status,"bad number of input strings")
@@ -20,7 +14,7 @@ test_that("doReaction fails gracefully",{
 test_that("replicator replicates",{
 
   result <- doReaction(c("OOGEOLHHHRLUEUOBBBRBXUUUDYGRHBLROORE$BLUBO^B>C$=?>$$BLUBO%}OYHOB","OOGEOLHHHRLUEUOBBBRBXUUUDYGRHBLROORE$BLUBO^B>C$=?>$$BLUBO%}OYHOB"))
-
+  expect_equal(result$bprob, 0.576381, tolerance=1e-6)
   expect_match(result$product,"OOGEOLHHHRLUEUOBBBRBXUUUDYGRHBLROORE$BLUBO^B>C$=?>$$BLUBO%}OYHOB")
 
 })
