921e2fdff416eb218b626487cb457026db25881a
howbuildingsfail <howbuildingsfail@gmail.com>
2019-07-08 23:28:30 +0100

    tidy reactiontype figs

--- examples/ReactionTypes.R
+++ examples/ReactionTypes.R
@@ -4,8 +4,8 @@
 
 
 #froot="D:/sjh/stringmol/out3smsp/"
-froot="D:/sjh/stringmol/smsp/1705smsp/out5/"
-#froot="~/Desktop/paulien/smsp/1705smsp/out3/"
+#froot="D:/sjh/stringmol/smsp/1705smsp/out5/"
+froot="~/Desktop/paulien/smsp/1705smsp/out5/"
 
 #pdf(file = "~/smsp5types.pdf",width = 12,height = 10)
 
@@ -17,14 +17,16 @@ gotdata<-T
 imno = 1
 while(gotdata){
 
-  png(file = sprintf("%srtypes%07d.png",froot,imno),width = 500,height=500)
+  png(file = sprintf("%srtypes%07d.png",froot,imno),width = 900,height=400)
 
+  par(mfrow=c(1,2),  bg = "grey40", fg = "white", col.lab = "white", col.axis = "white", col.main = "white")
 
   fn <- sprintf("%sout1_%d.conf",froot,tt)
 
-  message(sprintf("Working on file %s",fn))
 
   if(file.exists(fn)){
+
+    message(sprintf("Working on file %s",fn))
 
     #Get the data...
     data <- rconf_rdata(fn,summarize = F, verbose = F)#T)
@@ -42,29 +44,35 @@ while(gotdata){
       }
     }
 
-  #TODO: PLOT THE UNBOUND MOLECULES
+    #TODO: PLOT THE UNBOUND MOLECULES
 
 
-  # Now do the plot...
-  ps <- rconf_params(fn)
+    # Now do the plot...
+    ps <- rconf_params(fn)
 
-  plot(NA,xlim=c(0,ps$gridx+50),ylim=c(0,ps$gridy),asp=1,main=sprintf("T= %d",tt))
-  rect(xleft = 0,xright=ps$gridx,ybottom = 0,ytop=ps$gridy,col="black",lty = "blank")
+    plot(NA,xlim=c(0,125),ylim=c(0,100),asp=1,main=sprintf("T= %d",tt))
+    rect(xleft = 0,xright=ps$gridx,ybottom = 0,ytop=ps$gridy,col="black",lty = "blank")
 
-  for(mm in 1:nrow(data)){
-  xx <- data$actx[mm]
-  yy <- data$acty[mm]
-  rc <- rcols[which(rtypes == data$type[mm])]
-  rect(xleft = xx,xright = xx+1,ybottom = yy,ytop=yy+1,col=rc,lty = "blank")
-  xx <- data$passx[mm]
-  yy <- data$passy[mm]
-  rect(xleft = xx,xright = xx+1,ybottom = yy,ytop=yy+1,col=rc,lty = "blank")
-  legend("topright",pch=15,col = rcols,legend=rtypes)
+    for(mm in 1:nrow(data)){
+      xx <- data$actx[mm]
+      yy <- data$acty[mm]
+      rc <- rcols[which(rtypes == data$type[mm])]
+      rect(xleft = xx,xright = xx+1,ybottom = yy,ytop=yy+1,col=rc,lty = "blank")
+      xx <- data$passx[mm]
+      yy <- data$passy[mm]
+      rect(xleft = xx,xright = xx+1,ybottom = yy,ytop=yy+1,col=rc,lty = "blank")
+    }
+    #legend("topright",pch=15,col = rcols,legend=rtypes)
 
-  }
+
+    # NOW THE BARPLOT:
+    par(mar=c(10,6,1,0))
+    ttt <- table(data$type)
+    ttt <- ttt[rtypes]
+    barplot(ttt,las=2,col=rcols,ylim=c(0,2000))
   }
   else{
-  gotdata<-F
+    gotdata<-F
   }
   dev.off()
   tt <- tt+tstep
