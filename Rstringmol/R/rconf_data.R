2bdf393c79e0577eb16416a68ed161840f37b56c
howbuildingsfail <howbuildingsfail@gmail.com>
2019-08-06 09:45:52 +0100

    stash before merge

--- Rstringmol/R/rconf_data.R
+++ Rstringmol/R/rconf_data.R
@@ -46,79 +46,87 @@ rconf_rdata <- function(fn,verbose = F,summarize=F){
   count_bad_format = 0
 
   #build the data for each reaction - similar to the splist analysis
-  for(rr in 1:nr){
-    words <-strsplit(cf[rpos[rr]+2,1],split=" ")
-    actno[rr] <- as.numeric(words[[1]][3])
-    actseq[rr] <- words[[1]][4]
+  if(nr>0){
+  	if(verbose)message("Checking Reactions")
+	  for(rr in 1:nr){
+	    words <-strsplit(cf[rpos[rr]+2,1],split=" ")
+	    actno[rr] <- as.numeric(words[[1]][3])
+	    actseq[rr] <- words[[1]][4]
 
-    words <-strsplit(cf[rpos[rr]+3,1],split=" ")
-    actx[rr] <- as.numeric(words[[1]][19])
-    acty[rr] <- as.numeric(words[[1]][20])
+	    words <-strsplit(cf[rpos[rr]+3,1],split=" ")
+	    actx[rr] <- as.numeric(words[[1]][19])
+	    acty[rr] <- as.numeric(words[[1]][20])
 
-    foundpassive <- F
+	    foundpassive <- F
 
-    if(str_length(cf[rpos[rr]+7,1])>14){
 
-      words <-strsplit(cf[rpos[rr]+7,1],split=" ")
 
-      if(verbose)message(sprintf("Act: rr = %d Searching line %d:\n\t\t%s",rr,rpos[rr]+7, cf[rpos[rr]+7,1]))
-      ##if(is.na(as.integer(words[[1]][3])))message("BOOM")
 
-      if(words[[1]][1] == "###############"){
+	    if(rpos[rr]+7 < nrow(cf)){
+		    if(str_length(cf[rpos[rr]+7,1])>14){
 
-        ##if(pdebug)message(sprintf("words[[1]][3] = %s",words[[1]][3]))
-        ##TODO: we get 'NAs introduced by coercion' when this is e.g. "$OYHOB"
-        pasno[rr] <- suppressWarnings(  as.numeric(words[[1]][3]) )
-        passeq[rr] <- words[[1]][4]
+		      words <-strsplit(cf[rpos[rr]+7,1],split=" ")
 
-        words <-strsplit(cf[rpos[rr]+6,1],split=" ")
-        passx[rr] <- as.numeric(words[[1]][5])
-        passy[rr] <- as.numeric(words[[1]][6])
+		      if(verbose)message(sprintf("Act: rr = %d Searching line %d:\n\t\t%s",rr,rpos[rr]+7, cf[rpos[rr]+7,1]))
+		      ##if(is.na(as.integer(words[[1]][3])))message("BOOM")
 
-        foundpassive <- T
-      }
-    }
+		      if(words[[1]][1] == "###############"){
 
-    if(!foundpassive){
+			##if(pdebug)message(sprintf("words[[1]][3] = %s",words[[1]][3]))
+			##TODO: we get 'NAs introduced by coercion' when this is e.g. "$OYHOB"
+			pasno[rr] <- suppressWarnings(  as.numeric(words[[1]][3]) )
+			passeq[rr] <- words[[1]][4]
 
-      count_bad_format <- count_bad_format + 1
-      if(verbose)#message(sprintf("Bad format number %d encountered, rpos[rr] is %d, rr is %d, no is %d ax,ay = %d,%d", count_bad_format, rpos[rr], rr, pasno[rr],actx[rr],acty[rr]))
-        message(sprintf("Bad format number %d encountered",count_bad_format))
+			words <-strsplit(cf[rpos[rr]+6,1],split=" ")
+			passx[rr] <- as.numeric(words[[1]][5])
+			passy[rr] <- as.numeric(words[[1]][6])
 
-      #foundpassive <- F
-      loff <- -1
-      while(!foundpassive){
-        lno <- rpos[rr]+loff
-        if(str_length(cf[lno,1])>14){
-          if(verbose)message(sprintf("rr = %d Searching line %d:\n\t\t%s",rr,lno, cf[lno,1]))
+			foundpassive <- T
+		      }
+		    }
+		}
 
-          words <-strsplit(cf[lno,1],split=" ")
-          if(verbose)for(ww in 1:length(words[[1]]))message(sprintf("words[[1]][%d] is %s",ww,words[[1]][ww]))
-          if(words[[1]][1] == "###############"){
-            if(verbose)message("Found passive line...")
-            foundpassive <- T
-          }
-        }
-        loff <- loff-1
-        if(loff < -10){
-          message(sprintf("Unable to find passive partner; returning from rconf_data"))
-          return(NA)
-        }
-      }
+	    if(!foundpassive){
 
-      #TODO handle bad format better where passive mol is above rpos!! (and find out how this happens...)
-      #if(is.na(pasno[rr])){
-      pasno[rr] <- as.numeric(words[[1]][3])
-      passeq[rr] <- words[[1]][4]
-      #go up one more line to get gridx and gridy
-      words <-strsplit(cf[lno-1,1],split=" ")
-      passx[rr] <- as.numeric(words[[1]][5])
-      passy[rr] <- as.numeric(words[[1]][6])
+	      count_bad_format <- count_bad_format + 1
+	      if(verbose)#message(sprintf("Bad format number %d encountered, rpos[rr] is %d, rr is %d, no is %d ax,ay = %d,%d", count_bad_format, rpos[rr], rr, pasno[rr],actx[rr],acty[rr]))
+		message(sprintf("Bad format number %d encountered",count_bad_format))
 
-      if(verbose)message(sprintf("Found passeq:%s, passx:%s, passy:%s",passeq[rr],passx[rr],passy[rr]))
-    }
-    #message(sprintf("Active  molecule %s seq %s",actno[rr],actseq[rr]))
-    #message(sprintf("Passive molecule %s seq %s",actno[rr],actseq[rr]))
+	      #foundpassive <- F
+	      loff <- -1
+	      while(!foundpassive){
+		lno <- rpos[rr]+loff
+		if(str_length(cf[lno,1])>14){
+		  if(verbose)message(sprintf("rr = %d Searching line %d:\n\t\t%s",rr,lno, cf[lno,1]))
+
+		  words <-strsplit(cf[lno,1],split=" ")
+		  if(verbose)for(ww in 1:length(words[[1]]))message(sprintf("words[[1]][%d] is %s",ww,words[[1]][ww]))
+		  if(words[[1]][1] == "###############"){
+		    if(verbose)message("Found passive line...")
+		    foundpassive <- T
+		  }
+		}
+		loff <- loff-1
+		if(loff < -10){
+		  message(sprintf("Unable to find passive partner; returning from rconf_data"))
+		  return(NA)
+		}
+	      }
+
+	      #TODO handle bad format better where passive mol is above rpos!! (and find out how this happens...)
+	      #if(is.na(pasno[rr])){
+	      pasno[rr] <- as.numeric(words[[1]][3])
+	      passeq[rr] <- words[[1]][4]
+	      #go up one more line to get gridx and gridy
+	      words <-strsplit(cf[lno-1,1],split=" ")
+	      passx[rr] <- as.numeric(words[[1]][5])
+	      passy[rr] <- as.numeric(words[[1]][6])
+
+	      if(verbose)message(sprintf("Found passeq:%s, passx:%s, passy:%s",passeq[rr],passx[rr],passy[rr]))
+	    }
+	    #message(sprintf("Active  molecule %s seq %s",actno[rr],actseq[rr]))
+	    #message(sprintf("Passive molecule %s seq %s",actno[rr],actseq[rr]))
+	  }
   }
 
   eachreaction<-data.frame(actno,actseq,actx,acty,pasno,passeq,passx,passy,stringsAsFactors = F)
