a7626f8fd7ecd6b5c1c0c27d4ad671d7e028c201
howbuildingsfail <howbuildingsfail@gmail.com>
2019-12-02 23:13:49 +0000

    working on reaction properties; migrating to new laptop

--- Rstringmol/tests/testthat/test-reactionClassification.R
+++ Rstringmol/tests/testthat/test-reactionClassification.R
@@ -9,30 +9,37 @@ test_that("self-self reactions are classified correctly",{
 
   mA <- "ABAAABAA"
   m1 <- "NMMMNNNN"
+  np1 <- "$=?>G^AQC"
+  rep1 <- "$=?>G^AQC$=?>G^BQC$=?>E$BLUO%}PYH"
 
-  result <- doReaction(c(mA,m1))
-  conv <- doReaction(c(m1,mA))
-
-  expect_equal(result$bprob, 0.458261, tolerance=1e-6)
-  expect_true(result$product == "empty")
 
   # SELF-SELF Reactions
-  # Test no product
-  typ <- reaction_type(mA,m1,result,NA)
-  expect_true(typ$type == "NonSelfNoProduct",info = sprintf("no bind: output is \"%s\"",typ$type))
-
   # Test no bind
-  dummy <- result
-  dummy$bprob <- 0
-  dummy$mActive = "MACROMUTATION"
-  typ <- reaction_type(mA,m1,dummy,NA)
-  expect_true(typ$type == "Macromutation",info = sprintf("no bind: output is \"%s\"",typ$type))
+  result <- doReaction(c(mA,mA))
+  typ <- reaction_type(mA,mA)
+  expect_true(typ$type == "SelfSelfNoProduct",info = sprintf("no bind: output is \"%s\"",typ$type))
 
-  # Test self-self-replicator
-  rep1 <- "$=?>G^AQC$=?>G^BQC$=?>E$BLUO%}PYH"
-  result <- runReactionFP(c(rep1,rep1))
-  typ <- reaction_type(rep1,rep1,result,NA)
-  expect_true(typ$type == "SelfSelfReplicator",info = sprintf("no bind: output is \"%s\"",typ$type))
+  # Test replicator
+  result <- doReaction(c(rep1,rep1))
+  typ <- reaction_type(rep1,rep1)
+  expect_true(typ$type == "SelfSelfReplicator",info = sprintf("replictor: output is \"%s\"",typ$type))
+
+  # Test no product
+  result <- doReaction(c(np1,np1))
+  typ <- reaction_type(np1,np1,result)
+  expect_true(typ$type == "SelfSelfNoProduct",info = sprintf("no product: output is \"%s\"",typ$type))
+
+  # Test replicator different product
+  result <- doReaction(c(rep1,rep1))
+  result$product = "DIFFERENTPRODUCT"
+  typ <- reaction_type(rep1,rep1,result)
+  expect_true(typ$type == "SelfSelfDifferentProduct",info = sprintf("different product: output is \"%s\"",typ$type))
+
+  # Test Macromutation
+  result <- doReaction(c(rep1,rep1))
+  result$mActive = "MACROMUTATION"
+  typ <- reaction_type(rep1,rep1,result)
+  expect_true(typ$type == "Macromutation",info = sprintf("macromutation: output is \"%s\"",typ$type))
 
 })
 
@@ -51,19 +58,31 @@ test_that("non-self reactions are classified correctly",{
   # Simple way is to set bprob to zero
   dummy <- result
   dummy$bprob <- 0
-  typ <- reaction_type(mA,m1,dummy,NA)
+  typ <- reaction_type(mA,m1,dummy)
   expect_true(typ$type == "NonSelfNoProduct",info = sprintf("no bind: output is \"%s\"",typ$type))
 
   # Test replicator
   rep1 <- "$=?>G^AQC$=?>G^BQC$=?>E$BLUO%}PYH"
-  rep2 <- "$=?>G^AQC$=?>G^BQC$=?>E$BLUO%}PYHPYH"
+  rep2 <- "$=?>G^AQD$=?>G^BQC$=?>E$BLUO%}PYH"
   result <- runReactionFP(c(rep1,rep2))
-  typ <- reaction_type(rep1,rep2,result,NA)
+  conv <- runReactionFP(c(rep2,rep1))
+  typ <- reaction_type(rep1,rep2,result,conv)
   expect_true(typ$type == "NonSelfReplicator",info = sprintf("non-self replicator: output is \"%s\"",typ$type))
+
+
+  # Test parasite
+  rep1 <- "$=?>$EBUB^B$=?>$AVBO%}HOB"
+  rep2 <- "$=?>$B"
+  result <- runReactionFP(c(rep1,rep2))
+  conv <- runReactionFP(c(rep2,rep1))
+  typ <- reaction_type(rep1,rep2,result,conv)
+  expect_true(typ$type == "Parasite",info = sprintf("non-self replicator: output is \"%s\"",typ$type))
+
+
 
   # Test no product
   result <- runReactionFP(c(mA,m1))
-  typ <- reaction_type(mA,m1,result,NA)
+  typ <- reaction_type(mA,m1,result)
   expect_true(typ$type == "NonSelfNoProduct",info = sprintf("no bind: output is \"%s\"",typ$type))
 
   # Test different product (faking it)
@@ -71,8 +90,8 @@ test_that("non-self reactions are classified correctly",{
   rep2 <- "$=?>G^AQC$=?>G^BQC$=?>E$BLUO%}PYHPYH"
   result <- runReactionFP(c(rep1,rep2))
   result$product <- "DIFFERENT^PRODUCT"
-  typ <- reaction_type(rep1,rep2,result,NA)
-  expect_true(typ$type == "NonSelfdiffp",info = sprintf("non-self replicator: output is \"%s\"",typ$type))
+  typ <- reaction_type(rep1,rep2,result)
+  expect_true(typ$type == "NonSelfDifferentProduct",info = sprintf("non-self replicator: output is \"%s\", product is \"%s\"",typ$type,result$product))
 
 
 
