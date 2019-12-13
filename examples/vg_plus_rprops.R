9f3a7a61c80483067021c10a49bff2efd89bd3b3
Simon Hickinbotham <sjh518@york.ac.uk>
2019-12-13 14:41:22 +0000

    New reaction properties, but more changes to come!

--- examples/vg_plus_rprops.R
+++ examples/vg_plus_rprops.R
@@ -46,8 +46,8 @@ sm_lap_plot <- function(data,xlim=range(c(data$actlen,data$paslen)),title=""){
 
  data <- data[order(data$actlen),]
 
-
- pcols <- adjustcolor(rainbow(n = nrow(data)), alpha.f = 0.4)
+ pcols <- rainbow(n = nrow(data), alpha = 0.4)
+ #pcols <- adjustcolor(pcols, alpha.f = 0.4)
 
 
   plot(NA,xlim=xlim,ylim=c(5,25),yaxt="n",xlab="length",ylab ="",axes = F)
@@ -69,7 +69,7 @@ parasite_plot_old <- function(infroot,outfn,title="",xlim = c(0,100),from = 2000
 	xmax <- 100
         maxfreq <- 0
 
-	pdf(file=outfn,width = 9, height = 9)
+	pdf(file=outfn,width = 9, height = 12)
 	#for(tt in seq(from = 20000, to = 2000000, by = 20000)){
 	for(tt in seq(from = from, to = to, by = by)){
 	  message(sprintf("%d",tt))
@@ -89,7 +89,7 @@ parasite_plot_old <- function(infroot,outfn,title="",xlim = c(0,100),from = 2000
 		  ur$actlen <- str_length(ur$actseq)
 		  ur$paslen <- str_length(ur$passeq)
 
-		  par(mfrow=c(7,1))
+		  par(mfrow=c(9,1))
 		  par(mar=c(3,5,1,1))
 		  afreq <- sm_barplot(ur$actlen,xlim=xlim,xlab="length",ylab="frequency",
                                      title=sprintf("%s,T=%d\nAll Active Spp",title,tt),
@@ -158,7 +158,13 @@ parasite_plot_old <- function(infroot,outfn,title="",xlim = c(0,100),from = 2000
 }
 
 
+pairwise.properties <- function(fn){
 
+
+
+
+
+}
 
 
 
@@ -168,34 +174,41 @@ parasite_plot <- function(infroot,outfn,title="",xlim = c(0,100),from = 20000, t
 	xmax <- 100
     maxfreq <- 0
 
-	pdf(file=outfn,width = 9, height = 9)
+	pdf(file=outfn,width = 9, height = 11)
 	for(tt in seq(from = from, to = to, by = by)){
 	  message(sprintf("%d",tt))
-	  
+
 	  fn <- sprintf("%s%d.conf",infroot,tt)
 	  if(file.exists(fn)){
 
 		  fdata <- rconf_rdata(fn)
+		  fdata$actseq <- as.character(fdata$actseq)
+		  fdata$passeq <- as.character(fdata$passeq)
 
           #create a data frame of unique reactions for this file:
-		  ur <- unique(data.frame(actseq = fdata$actseq, passeq = fdata$passeq))
-		  ur$actseq <- as.character(ur$actseq)
-		  ur$passeq <- as.character(ur$passeq)
+		  ur <- unique(data.frame(actseq = fdata$actseq, passeq = fdata$passeq, stringsAsFactors = F))
+		  #ur$actseq <- as.character(ur$actseq)
+		  #ur$passeq <- as.character(ur$passeq)
+
+
 		  ur$type <- "unknown"
 		  ur$product <- ""
-		  
-		  
+
+		  #These are the properties agreed with Susan
 		  ur$pp_obligate <- F
-		  
-		  
-		  
-		  
-		  
+		  ur$pp_NoProduct <-F
+		  ur$pp_NewProduct <-F
+		  ur$pp_Replicator <-F
+		  ur$pp_Hypercycle <-F
+		  ur$pp_Parasitic <-F
+		  ur$pp_Complex <-F
+		  ur$pp_Jumper <-F
+
 
 		  ur$actlen <- str_length(ur$actseq)
 		  ur$paslen <- str_length(ur$passeq)
 
-		  par(mfrow=c(7,1))
+		  par(mfrow=c(9,1))
 		  par(mar=c(3,5,1,1))
 		  afreq <- sm_barplot(ur$actlen,xlim=xlim,xlab="length",ylab="frequency",
                                      title=sprintf("%s,T=%d\nAll Active Spp",title,tt),
@@ -205,46 +218,106 @@ parasite_plot <- function(infroot,outfn,title="",xlim = c(0,100),from = 20000, t
 		  maxfreq <- max(maxfreq,afreq,pfreq)
 
 		  for (rr in 1:nrow(ur)){
-		    rt <- reaction_type(ur$actseq[rr],ur$passeq[rr])
-		    #message(sprintf("%d: %s %s - Reaction Type = %s",rr,ur$actseq[rr],ur$passeq[rr],rt$type))
-		    ur$type[rr] <- rt$type
-		    ur$product[rr] <- rt$product
-		    ur$dexec[rr] <- rt$deterministicExec
-		    ur$dbind[rr] <- rt$deterministicBind
-		    ur$nobs[rr] <- nrow(fdata[fdata$actseq == ur$actseq[rr] & fdata$passeq == ur$passeq[rr],])
+		    #################################################
+                    # Reaction properties
+
+		    #rt <- reaction_type(ur$actseq[rr],ur$passeq[rr])
+		    # Run the reaction(s):
+		    result <- runReactionFP(c(ur$actseq[rr],ur$passeq[rr]))
+		    conv <- result
+		    if(ur$actseq[rr] != ur$passeq[rr]){
+		      if(result$deterministicBind){
+		 	ur$pp_Obligate <- T
+			conv <- NULL
+
+		      }
+		      else{
+			conv <- runReactionFP(c(ur$passeq[rr],ur$actseq[rr]))
+		      }
+		    }
+
+		    if(result$product == "empty")
+			ur$pp_NoProduct[rr] <- T
+		    else{
+		    	if(result$product != ur$actseq[rr] & result$product != ur$passeq[rr])
+				ur$pp_NewProduct[rr] <- T
+			}
+
+		    if(!is.null(conv)){
+		    	#Replicator / Hypercycle
+			if((result$product == ur$passeq[rr] & conv$product == ur$actseq[rr])
+					|
+			   (result$product == ur$actseq[rr] & conv$product == ur$passeq[rr])){
+				ur$pp_Replicator[rr] <- T
+		                if(ur$actseq[rr] != ur$passeq[rr])
+				  ur$pp_Hypercycle[rr] <- T
+			    }
+		        #Parasite
+		        if(result$product == ur$passeq[rr] & conv$product != ur$actseq[rr])
+			    ur$pp_Parasitic[rr] <- T
+		        }
+		    else{
+			if((result$product == ur$passeq[rr])
+                        #     |
+		        #   (result$product == ur$actseq[rr])
+			){
+			    ur$pp_Parasitic[rr] <- T
+			}
+		    }
+
+
+
+#message("Complex?")
+		    #Complex
+		    if(result$mActive != ur$actseq[rr] | result$mPassive != ur$passeq[rr])
+			ur$pp_Complex[rr] <- T
+
+		    #Jumper
+		    if(result$mActive == "" | result$mPassive == "")
+			ur$pp_Jumper[rr] <- T
+
+
+#message("product")
+
+		    #################################################
+		    #ur$type[rr] <- "notyp
+		    ur$product[rr] <- result$product
+		    ur$dexec[rr] <- result$deterministicExec
+		    ur$dbind[rr] <- result$deterministicBind
+		    ur$nobs[rr] <- nrow(fdata[fdata$actseq == ur$actseq[rr]
+                                      & fdata$passeq == ur$passeq[rr],])
+#message("next")
 		  }
 
 
-		  #ur$actlen <- as.integer(str_length(pur$actseq))
-		  #ur$paslen <- as.integer(str_length(pur$passeq))
-
-		  pur <- rbind(ur[ur$type=="SelfSelfReplicator",] , ur[ur$type=="NonSelfReplicator" ,])
-		  sm_lap_plot(pur,xlim=xlim,title=sprintf("Replicator reactions"))
+		  pur <- rbind(ur[ur$pp_Replicator,])
+		  sm_lap_plot(pur,xlim=xlim,title=sprintf("Replicator reactions (n=%d)",sum(pur$nobs)))
 		  smlur <- pur[str_length(pur$passeq)<20,]
-                  #if(nrow(smlur>0)){
-                  #  message("SHORT REPLICATORS!")
-                  #  for(rr in 1:nrow(smlur)){
-                  #    print_mols("SHORT",smlur$actseq[rr],smlur$passeq[rr],"","")
-                  #  }
-                  #}
+
+		  pur <- ur[ur$pp_Parasitic,]
+		  sm_lap_plot(pur,xlim=xlim,title=sprintf("Parasitic reactions (n=%d)",sum(pur$nobs)))
 
 
-
-		  pur <- ur[ur$type=="Parasite",]
-		  sm_lap_plot(pur,xlim=xlim,title=sprintf("Parasites"))
+		  pur <- rbind(ur[ur$pp_Hypercycle,])
+		  sm_lap_plot(pur,xlim=xlim,title=sprintf("Hypercycle (n=%d)",sum(pur$nobs)))
 
 		  #nur <- ur[ur$type!="Parasite",]
 		  #sm_lap_plot(nur,xlim=xlim)
 		  #title(sprintf("Non-parasitic reactions"))
 
-		  pur <- rbind(ur[ur$type=="SelfSelfNoProduct",] ,  ur[ur$type=="NonSelfNoProduct" ,])
-		  sm_lap_plot(pur,xlim=xlim,title=sprintf("Reactions with no product"))
+		  pur <- rbind(ur[ur$pp_NoProduct,])
+		  sm_lap_plot(pur,xlim=xlim,title=sprintf("Reactions with no product (n=%d)",sum(pur$nobs)))
 
-		  pur <- ur[ur$type=="Macromutation",]
-		  sm_lap_plot(pur,xlim=xlim,title=sprintf("NonCatalytic reactions"))
+		  pur <- ur[ur$pp_Complex,]
+		  sm_lap_plot(pur,xlim=xlim,title=sprintf("Complex reactions (n=%d)",sum(pur$nobs)))
 
-		  pur <- rbind(ur[ur$type=="SelfSelfDifferentProduct",] , ur[ur$type=="NonSelfDifferentProduct" ,])
-		  sm_lap_plot(pur,xlim=xlim,title=sprintf("Reactions with a different product"))
+		  pur <- rbind(ur[ur$pp_NewProduct,])
+		  sm_lap_plot(pur,xlim=xlim,title=sprintf("New product (n=%d)",sum(pur$nobs)))
+
+
+		  pur <- rbind(ur[ur$pp_Jumper,])
+		  sm_lap_plot(pur,xlim=xlim,title=sprintf("Jumper (n=%d)",sum(pur$nobs)))
+
 
 		  rm(ur)
 		  rm(fdata)
@@ -271,27 +344,27 @@ parasite_plot("~/Desktop/paulien/smsp/1705smspr/out5/out1_","smspr_out5.pdf",tit
 
 if(!testing){
 	#box-box
-	parasite_plot("~/Desktop/paulien/smsp/1705smsp/out1/out1_","smsp_out1.pdf",title="box-box1")
-	parasite_plot("~/Desktop/paulien/smsp/1705smsp/out2/out1_","smsp_out2.pdf",title="box-box2")
-	parasite_plot("~/Desktop/paulien/smsp/1705smsp/out3/out1_","smsp_out3.pdf",title="box-box3")
-	parasite_plot("~/Desktop/paulien/smsp/1705smsp/out5/out1_","smsp_out5.pdf",title="box-box5")
+	parasite_plot("~/Desktop/paulien/smsp/1705smsp/out1/out1_","smsp_out1n.pdf",title="box-box1")
+	parasite_plot("~/Desktop/paulien/smsp/1705smsp/out2/out1_","smsp_out2n.pdf",title="box-box2")
+	parasite_plot("~/Desktop/paulien/smsp/1705smsp/out3/out1_","smsp_out3n.pdf",title="box-box3")
+	parasite_plot("~/Desktop/paulien/smsp/1705smsp/out5/out1_","smsp_out5n.pdf",title="box-box5")
 
 	#box-rand
-	parasite_plot("~/Desktop/paulien/smsp/1705smspr/out2/out1_","smspr_out2.pdf",title="box-rand2")
-	parasite_plot("~/Desktop/paulien/smsp/1705smspr/out3/out1_","smspr_out3.pdf",title="box-rand3")
+	parasite_plot("~/Desktop/paulien/smsp/1705smspr/out2/out1_","smspr_out2n.pdf",title="box-rand2")
+	parasite_plot("~/Desktop/paulien/smsp/1705smspr/out3/out1_","smspr_out3n.pdf",title="box-rand3")
 
 	#slot-box
-	parasite_plot("~/Desktop/paulien/smsp/1705sm250/out4/out1_","sm250_out4.pdf",title="slot-box4")
-	parasite_plot("~/Desktop/paulien/smsp/1705sm250/out5/out1_","sm250_out5.pdf",title="slot-box5")
+	parasite_plot("~/Desktop/paulien/smsp/1705sm250/out4/out1_","sm250_out4n.pdf",title="slot-box4")
+	parasite_plot("~/Desktop/paulien/smsp/1705sm250/out5/out1_","sm250_out5n.pdf",title="slot-box5")
 
 	#slot-rand
-	parasite_plot("~/Desktop/paulien/smsp/1705sm250r/out3/out1_","sm250r_out3.pdf",title="slot-box4",to=340000)
+	parasite_plot("~/Desktop/paulien/smsp/1705sm250r/out3/out1_","sm250r_out3n.pdf",title="slot-box4",to=340000)
 
 	#evoevo
-	parasite_plot("~/Desktop/paulien/smsp/evoevo/out1/out1_","evoevo_out1.pdf",title="evoevo1",from=1000000)
-	parasite_plot("~/Desktop/paulien/smsp/evoevo/out2/out1_","evoevo_out2.pdf",title="evoevo2",from=1000000)
-	parasite_plot("~/Desktop/paulien/smsp/evoevo/out3/out1_","evoevo_out3.pdf",title="evoevo3",from=1000000)
-	parasite_plot("~/Desktop/paulien/smsp/evoevo/out4/out1_","evoevo_out4.pdf",title="evoevo4",from=1000000)
-	parasite_plot("~/Desktop/paulien/smsp/evoevo/out5/out1_","evoevo_out5.pdf",title="evoevo5",from=1000000)
+	parasite_plot("~/Desktop/paulien/smsp/evoevo/out1/out1_","evoevo_out1n.pdf",title="evoevo1",from=1000000)
+	parasite_plot("~/Desktop/paulien/smsp/evoevo/out2/out1_","evoevo_out2n.pdf",title="evoevo2",from=1000000)
+	parasite_plot("~/Desktop/paulien/smsp/evoevo/out3/out1_","evoevo_out3n.pdf",title="evoevo3",from=1000000)
+	parasite_plot("~/Desktop/paulien/smsp/evoevo/out4/out1_","evoevo_out4n.pdf",title="evoevo4",from=1000000)
+	parasite_plot("~/Desktop/paulien/smsp/evoevo/out5/out1_","evoevo_out5n.pdf",title="evoevo5",from=1000000)
 }
 
