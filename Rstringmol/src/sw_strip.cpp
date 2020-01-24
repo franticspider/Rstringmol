75b06d9f09676594234f537f1982b6c47fe57b2a
Simon Hickinbotham <sjh518@york.ac.uk>
2020-01-24 18:27:20 +0000

    Working version of sw_split now done. Commit now before doing the last thing for this - counting the number of matched characters

--- Rstringmol/src/sw_strip.cpp
+++ Rstringmol/src/sw_strip.cpp
@@ -12,7 +12,7 @@ typedef struct {
 
 
 
-bool procH(char l1, char *s2, int l2, float *H, swt *swT, swPos *swp){
+bool procH(char l1, char *s2, int l2, float *H, swt *swT, swPos *swp, bool forward = true){
 
   int jj,sj,oo = 0;
   float ms,is,ds,pmax=0.;
@@ -20,37 +20,74 @@ bool procH(char l1, char *s2, int l2, float *H, swt *swT, swPos *swp){
   float Hjminus1 = 0;
   bool updated = false;
 
-  for(jj=1;jj<=l2;jj++){
-  //for(j=1;j<=l2;j++){
-    sj=jj-1;
+  if(forward){
+    for(jj=1;jj<=l2;jj++){
+      sj=jj-1;
 
-    //Calculate the three scores:
+      //Calculate the three scores:
 
-    //Match/Mismatch score
-    ms = Hjminus1 + wT(l1,s2[sj],swT);
+      //Match/Mismatch score
+      ms = Hjminus1 + wT(l1,s2[sj],swT);
 
-    //Deletion score
-    ds = H[jj] + wT(l1,0,swT);
+      //Deletion score
+      ds = H[jj] + wT(l1,0,swT);
 
-    //Insertion score
-    is = H[jj-1] + wT(0,s2[sj],swT);
+      //Insertion score
+      is = H[jj-1] + wT(0,s2[sj],swT);
 
-    //record the proxy for H[ii-1][jj] - it'll be needed next time round as H[ii-1][jj-1]!
-    //DEBUG: Rprintf("s2[jj] = %c, H[jj] = %0.2f, hjminus1 = %0.2f\n",s2[sj],H[jj],Hjminus1);
-    Hjminus1 = H[jj];
+      //record the proxy for H[ii-1][jj] - it'll be needed next time round as H[ii-1][jj-1]!
+      //DEBUG: Rprintf("s2[jj] = %c, H[jj] = %0.2f, hjminus1 = %0.2f\n",s2[sj],H[jj],Hjminus1);
+      Hjminus1 = H[jj];
 
-    //update H for new row at jj
-    H[jj] = 0;
-    if(ms > H[jj])H[jj] = ms;
-    if(ds > H[jj])H[jj] = ds;
-    if(is > H[jj])H[jj] = is;
+      //update H for new row at jj
+      H[jj] = 0;
+      if(ms > H[jj])H[jj] = ms;
+      if(ds > H[jj])H[jj] = ds;
+      if(is > H[jj])H[jj] = is;
 
 
-    //record the posn of the highest score:
-    if(H[jj]>swp->maxscore){
-      swp->maxscore = H[jj];
-      swp->pos_jj=jj;
-      updated = true;
+      //record the posn of the highest score:
+      if(H[jj]>swp->maxscore){
+        swp->maxscore = H[jj];
+        swp->pos_jj=jj;
+        updated = true;
+      }
+    }
+  }
+  else{
+    for(jj=l2;jj>=1;jj--){
+      sj=jj+1;
+
+      //Calculate the three scores:
+
+      //Match/Mismatch score
+      ms = Hjminus1 + wT(l1,s2[jj-1],swT);
+
+      //Deletion score
+      ds = H[jj] + wT(l1,0,swT);
+
+      //Insertion score
+      is = H[sj] + wT(0,s2[jj-1],swT);
+
+      //record the proxy for H[ii-1][jj] - it'll be needed next time round as H[ii-1][jj-1]!
+      //DEBUG: Rprintf("s2[jj] = %c, H[jj] = %0.2f, hjminus1 = %0.2f\n",s2[sj],H[jj],Hjminus1);
+      Hjminus1 = H[jj];
+
+      //update H for new row at jj
+      H[jj] = 0;
+      if(ms > H[jj]){
+        H[jj] = ms;
+      }
+      if(ds > H[jj])H[jj] = ds;
+      if(is > H[jj])H[jj] = is;
+
+
+      //record the posn of the highest score:
+      if(H[jj]>swp->maxscore){
+        swp->maxscore = H[jj];
+        swp->pos_jj=jj;
+        updated = true;
+      }
     }
   }
 
@@ -64,10 +101,14 @@ swPos procStrip(char *s1, char *s2, align *A, swt *swT, int verbose){
   int l2 = strlen(s2);
   float *H;
 
-  swPos pos;
+  swPos pos,ros;
   pos.pos_ii = 0;
   pos.pos_jj = 0;
   pos.maxscore = 0.;
+
+  ros.pos_ii = 0;
+  ros.pos_jj = 0;
+  ros.maxscore = 0.;
 
   //Set up the array - TODO: declare globally if H is likely to be reused.
   H =(float *) malloc((l2+1)*sizeof(float));
@@ -98,15 +139,36 @@ swPos procStrip(char *s1, char *s2, align *A, swt *swT, int verbose){
       pos.pos_ii = i;
     }
 
-    print_Hrow(s1[i-1],&(H[1]),l2);
+    if(verbose)print_Hrow(s1[i-1],&(H[1]),l2);
+  }
+
+  if(verbose){
+    Rprintf("After forward, i,j and score are %d, %d and %0.2f\n",pos.pos_ii,pos.pos_jj,pos.maxscore);
+
+    /* print the "header row" of the alignment table (=s2) */
+    print_header(s2,l2);
   }
 
   //Now process in "reverse" from the end point to get the start point..
+  memset(H,0,(l2+1)*sizeof(float));
   for(i=pos.pos_ii;i>0;i--){
-    if(procH())
-
-
+    if(procH(s1[i-1],s2,pos.pos_jj,H,swT,&ros,false)){
+      ros.pos_ii = i;
+    }
+    if(verbose)print_Hrow(s1[i-1],&(H[1]),l2);
   }
+
+  if(verbose)Rprintf("After reverse, i,j and score are %d, %d and %0.2f\n",ros.pos_ii,ros.pos_jj,ros.maxscore);
+
+  A->s1 = ros.pos_ii-1;
+  A->s2 = ros.pos_jj-1;
+  A->e1 = pos.pos_ii;
+  A->e2 = pos.pos_jj;
+  A->score = pos.maxscore;
+  //Note that this is an upper bound on the match, but we don't use it anyway!
+  A->match = (A->e1 - A->s1) > (A->e2 - A->s2) ? (A->e1 - A->s1) : (A->e2 - A->s2);
+  A->prob = 0.0; //TODO: should really use get_bprob() here - would be clearer!
+
 
 
   free(H);
--- examples/vg_plus_rprops.R
+++ examples/vg_plus_rprops.R
@@ -355,42 +355,46 @@ parasite_plot <- function(infroot,outfn,title="",xlim = c(0,100),from = 20000, t
 	dev.off()
 }
 
-network.plot <- function(infn,outfn){
-
-}
 
 
 
-
-testing <- T
-#parasite_plot("~/Desktop/paulien/smsp/1705smsp/out1/out1_","test_smsp_out1.pdf",title="box-box1",to=100000)
-parasite_plot("~/Desktop/paulien/smsp/1705smspr/out5/out1_","smspr_out5c.pdf",title="box-rand5")
+#set this to false if you want to run code... and True if you want to "source" it
+onlyfuncs <- T
 
 
-if(!testing){
-	#box-box
-	parasite_plot("~/Desktop/paulien/smsp/1705smsp/out1/out1_","csmsp_out1n.pdf",title="box-box1")
-	parasite_plot("~/Desktop/paulien/smsp/1705smsp/out2/out1_","csmsp_out2n.pdf",title="box-box2")
-	parasite_plot("~/Desktop/paulien/smsp/1705smsp/out3/out1_","csmsp_out3n.pdf",title="box-box3")
-	parasite_plot("~/Desktop/paulien/smsp/1705smsp/out5/out1_","csmsp_out5n.pdf",title="box-box5")
+if(!onlyfuncs){
+  
 
-	#box-rand
-	parasite_plot("~/Desktop/paulien/smsp/1705smspr/out2/out1_","csmspr_out2n.pdf",title="box-rand2")
-	parasite_plot("~/Desktop/paulien/smsp/1705smspr/out3/out1_","csmspr_out3n.pdf",title="box-rand3")
-
-	#slot-box
-	parasite_plot("~/Desktop/paulien/smsp/1705sm250/out4/out1_","csm250_out4n.pdf",title="slot-box4")
-	parasite_plot("~/Desktop/paulien/smsp/1705sm250/out5/out1_","csm250_out5n.pdf",title="slot-box5")
-
-	#slot-rand
-	parasite_plot("~/Desktop/paulien/smsp/1705sm250r/out3/out1_","csm250r_out3n.pdf",title="slot-box4",to=340000)
-
-	#evoevo
-	parasite_plot("~/Desktop/paulien/smsp/evoevo/out1/out1_","cevoevo_out1n.pdf",title="evoevo1",from=1000000)
-	parasite_plot("~/Desktop/paulien/smsp/evoevo/out2/out1_","cevoevo_out2n.pdf",title="evoevo2",from=1000000)
-	parasite_plot("~/Desktop/paulien/smsp/evoevo/out3/out1_","cevoevo_out3n.pdf",title="evoevo3",from=1000000)
-	parasite_plot("~/Desktop/paulien/smsp/evoevo/out4/out1_","cevoevo_out4n.pdf",title="evoevo4",from=1000000)
-	parasite_plot("~/Desktop/paulien/smsp/evoevo/out5/out1_","cevoevo_out5n.pdf",title="evoevo5",from=1000000)
+  testing <- T
+  #parasite_plot("~/Desktop/paulien/smsp/1705smsp/out1/out1_","test_smsp_out1.pdf",title="box-box1",to=100000)
+  parasite_plot("~/Desktop/paulien/smsp/1705smspr/out5/out1_","smspr_out5c.pdf",title="box-rand5")
+  
+  
+  if(!testing){
+  	#box-box
+  	parasite_plot("~/Desktop/paulien/smsp/1705smsp/out1/out1_","csmsp_out1n.pdf",title="box-box1")
+  	parasite_plot("~/Desktop/paulien/smsp/1705smsp/out2/out1_","csmsp_out2n.pdf",title="box-box2")
+  	parasite_plot("~/Desktop/paulien/smsp/1705smsp/out3/out1_","csmsp_out3n.pdf",title="box-box3")
+  	parasite_plot("~/Desktop/paulien/smsp/1705smsp/out5/out1_","csmsp_out5n.pdf",title="box-box5")
+  
+  	#box-rand
+  	parasite_plot("~/Desktop/paulien/smsp/1705smspr/out2/out1_","csmspr_out2n.pdf",title="box-rand2")
+  	parasite_plot("~/Desktop/paulien/smsp/1705smspr/out3/out1_","csmspr_out3n.pdf",title="box-rand3")
+  
+  	#slot-box
+  	parasite_plot("~/Desktop/paulien/smsp/1705sm250/out4/out1_","csm250_out4n.pdf",title="slot-box4")
+  	parasite_plot("~/Desktop/paulien/smsp/1705sm250/out5/out1_","csm250_out5n.pdf",title="slot-box5")
+  
+  	#slot-rand
+  	parasite_plot("~/Desktop/paulien/smsp/1705sm250r/out3/out1_","csm250r_out3n.pdf",title="slot-box4",to=340000)
+  
+  	#evoevo
+  	parasite_plot("~/Desktop/paulien/smsp/evoevo/out1/out1_","cevoevo_out1n.pdf",title="evoevo1",from=1000000)
+  	parasite_plot("~/Desktop/paulien/smsp/evoevo/out2/out1_","cevoevo_out2n.pdf",title="evoevo2",from=1000000)
+  	parasite_plot("~/Desktop/paulien/smsp/evoevo/out3/out1_","cevoevo_out3n.pdf",title="evoevo3",from=1000000)
+  	parasite_plot("~/Desktop/paulien/smsp/evoevo/out4/out1_","cevoevo_out4n.pdf",title="evoevo4",from=1000000)
+  	parasite_plot("~/Desktop/paulien/smsp/evoevo/out5/out1_","cevoevo_out5n.pdf",title="evoevo5",from=1000000)
+  }
 }
 
 
@@ -474,35 +478,41 @@ plot.rnet<- function(fn,onlyreplicators=T){
 
 }
 
-#Do a basic test:
-testinfn <- "~/Desktop/paulien/smsp/1705smspr/out5/out1_40000.conf"
-pdf(file = "testnet.pdf",width = 38,height = 38)
- plot.rnet(testinfn)
- title("\ntest title",cex.main=15)
-dev.off()
 
-#Now let's try a proper run:
-#parasite_plot("~/Desktop/paulien/smsp/1705smsp/out1/out1_","csmsp_out1n.pdf",title="box-box1")
-froot <- "~/Desktop/paulien/smsp/1705smsp/out2/out1_"
-pdf(file="csmsp_out2net.pdf",width=38,height=38)
-  for(tt in seq(from = 20000, to = 2000000, by = 20000)){
-    message(sprintf("%d",tt))
 
-    fn <- sprintf("%s%d.conf",froot,tt)
-    if(file.exists(fn)){
-      plot.rnet(fn,onlyreplicators = T)
-      title(sprintf("\nt=%d",tt),cex.main=15)
+
+if(!onlyfuncs){
+
+  #Do a basic test:
+  testinfn <- "~/Desktop/paulien/smsp/1705smspr/out5/out1_40000.conf"
+  pdf(file = "testnet.pdf",width = 38,height = 38)
+   plot.rnet(testinfn)
+   title("\ntest title",cex.main=15)
+  dev.off()
+  
+  #Now let's try a proper run:
+  #parasite_plot("~/Desktop/paulien/smsp/1705smsp/out1/out1_","csmsp_out1n.pdf",title="box-box1")
+  froot <- "~/Desktop/paulien/smsp/1705smsp/out2/out1_"
+  pdf(file="csmsp_out2net.pdf",width=38,height=38)
+    for(tt in seq(from = 20000, to = 2000000, by = 20000)){
+      message(sprintf("%d",tt))
+  
+      fn <- sprintf("%s%d.conf",froot,tt)
+      if(file.exists(fn)){
+        plot.rnet(fn,onlyreplicators = T)
+        title(sprintf("\nt=%d",tt),cex.main=15)
+      }
     }
-  }
-dev.off()
+  dev.off()
+  
+  
+  #Biofabric alternative
+  pdf(file = "testnetbf.pdf",width = 38,height = 38)
+  library(RBioFabric)
+  bioFabric(net)
+  dev.off()
 
-
-#Biofabric alternative
-pdf(file = "testnetbf.pdf",width = 38,height = 38)
-library(RBioFabric)
-bioFabric(net)
-dev.off()
-
+}
 
 
 
